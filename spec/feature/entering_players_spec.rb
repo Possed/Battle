95b2d31ec77718e9e9d6a39047ac971fcdb508c0
Possed <r0xana.pirvu@yahoo.com>
2018-05-02 15:03:26 +0100

    using web helpers

--- spec/feature/entering_players_spec.rb
+++ spec/feature/entering_players_spec.rb
@@ -1,10 +1,7 @@
 feature 'Testing player s names' do
 
   it 'displays the players names' do
-    visit('/')
-    fill_in :p1, with: 'Igor'
-    fill_in :p2, with: 'Roxana'
-    click_button :Submit
+    sign_in_and_play
     expect(page).to have_content "Player 1: Igor\nPlayer 2: Roxana"
   end
 end
--- spec/feature/viewing_hit_points_spec.rb
+++ spec/feature/viewing_hit_points_spec.rb
@@ -1,10 +1,7 @@
 feature 'Viewing hit points of player 2' do
 
   it 'displays the player 2\'s hit points' do
-    visit('/')
-    fill_in :p1, with: 'Igor'
-    fill_in :p2, with: 'Roxana'
-    click_button :Submit
+    sign_in_and_play
     expect(page).to have_content "Roxana HP 60"
   end
 end
